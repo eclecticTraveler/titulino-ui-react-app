3ccea0e0f855e97d8de0a7b73a428cbf57b0c96c
Alberto Arellano <are11001@byui.edu>
2024-09-08 23:54:33 -0600

    Pilot program, removing KC for now

--- src/App.js
+++ src/App.js
@@ -6,22 +6,15 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import { THEME_CONFIG } from './configs/AppConfig';
-import { ReactKeycloakProvider } from "@react-keycloak/web";
-import keycloak from "./auth/Keycloak"
 
 const themes = {
   dark: `${process.env.PUBLIC_URL}/css/dark-theme.css`,
   light: `${process.env.PUBLIC_URL}/css/light-theme.css`,
 };
 
-const keycloakProviderInitConfig = {
-  onLoad: 'login-required',
-}
-
 function App(){
     return (
       <div className="App">
-        <ReactKeycloakProvider authClient={keycloak} initConfig={keycloakProviderInitConfig}>
           <Provider store={store}>
             <ThemeSwitcherProvider themeMap={themes} defaultTheme={THEME_CONFIG.currentTheme} insertionPoint="styles-insertion-point">
               <Router>
@@ -31,7 +24,6 @@ function App(){
               </Router>
             </ThemeSwitcherProvider>
           </Provider>
-        </ReactKeycloakProvider>
       </div>
   );
 }
