2b9865dbdfca4e8544beda9f39b7db8384d2f701
Alberto Arellano <are11001@byui.edu>
2024-09-18 00:03:13 -0600

    fix some warnings from console

--- src/views/index.js
+++ src/views/index.js
@@ -1,100 +1,87 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppLocale from "../lang";
 import useBodyClass from "../hooks/useBodyClass";
 import { connect } from "react-redux";
 import { IntlProvider } from "react-intl";
 import { ConfigProvider } from "antd";
-import { DEFAULT_PREFIX_VIEW, APP_PREFIX_PATH, AUTH_PREFIX_PATH } from "../configs/AppConfig";
+import { DEFAULT_PREFIX_VIEW, APP_PREFIX_PATH } from "../configs/AppConfig";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 import AuthLayout from '../layouts/auth-layout';
 import AppLayout from "../layouts/app-layout";
-import RenderOnAuthenticated from '../components/RenderOnAuthenticated';
 import CourseSelection from './app-views/course-selection';
-import {getWasUserConfigSetFlag, getUserSelectedCourse, getUserNativeLanguage }  from '../redux/actions/Lrn';
+import { getWasUserConfigSetFlag, getUserSelectedCourse, getUserNativeLanguage } from '../redux/actions/Lrn';
 import { onLocaleChange, onCourseChange, onLoadingUserSelectedTheme } from '../redux/actions/Theme'
-import RenderOnlyOnAuthenticated from "../components/RenderOnlyOnAuthenticated";
-import { useKeycloak, KeycloakProvider } from "@react-keycloak/web";
 import { useThemeSwitcher } from "react-css-theme-switcher";
 import { bindActionCreators } from 'redux';
-  
+
 export const Views = (props) => { 
-	const { locale, location, direction, course, selectedCourse, getUserNativeLanguage, onLocaleChange, nativeLanguage,
-            wasUserConfigSet, getWasUserConfigSetFlag, getUserSelectedCourse, onCourseChange, currentTheme, onLoadingUserSelectedTheme, subNavPosition } = props;
+    const { locale, direction, course, selectedCourse, getUserNativeLanguage, onLocaleChange, nativeLanguage,
+        wasUserConfigSet, getWasUserConfigSetFlag, getUserSelectedCourse, onCourseChange, currentTheme, onLoadingUserSelectedTheme, subNavPosition } = props;
+
     const localization = locale ? locale : 'en';
     const currentAppLocale = AppLocale[localization];
     const { switcher, themes } = useThemeSwitcher();
- 
+
     useBodyClass(`dir-${direction}`);
 
-    // Load user selected theme
-    onLoadingUserSelectedTheme();
+    // Effect to load user selected theme and locale
+    useEffect(() => {
+        onLoadingUserSelectedTheme();
 
-    if(currentTheme){
-        switcher({ theme: themes[currentTheme] });
-    }
-
-
-    const repopulateConfiguration = () => {
-        // Changes lost on a refresh, order of these functions are important
-        getUserNativeLanguage(); 
-        if(nativeLanguage){
-            onLocaleChange(nativeLanguage?.localizationId)
+        if (currentTheme) {
+            switcher({ theme: themes[currentTheme] });
         }
-        
-        getUserSelectedCourse();    
-        if(selectedCourse){
-            onCourseChange(selectedCourse?.courseAbbreviation);
-        } 
-    }
 
-    getWasUserConfigSetFlag();
-    (wasUserConfigSet && !course) ?? repopulateConfiguration();    
-        return(    
-                <IntlProvider 			
-                    locale={currentAppLocale.locale}
-                    messages={currentAppLocale.messages}>
-                        <ConfigProvider locale={currentAppLocale.antd} direction={direction}>
-                                <Switch>
-                                    {(!wasUserConfigSet) &&                                 
-                                    <CourseSelection/>
-                                    }
-                                    <Route exact path="/">
-                                        <Redirect to={APP_PREFIX_PATH ? APP_PREFIX_PATH : DEFAULT_PREFIX_VIEW} />
-                                    </Route>
-                                    <Route path={APP_PREFIX_PATH ? APP_PREFIX_PATH : DEFAULT_PREFIX_VIEW}>
-                                        <AppLayout direction={direction} location={location}/>
-                                    </Route>
-                                    {/* <KeycloakProvider keycloak={keycloak} onEvent={this.keycloak.logout()}> */}
-                                        {/* <AppLayout direction={direction} location={location}/> */}
-                                    {/* </KeycloakProvider> */}
-                                    {/* <RenderOnlyOnAuthenticated> */}
-                                        {/* <AuthLayout direction={direction} /> */}
-                                        {/* <AppLayout direction={direction} location={location}/> */}
-                                    {/* </RenderOnlyOnAuthenticated> */}
-                                </Switch>                            
-                    </  ConfigProvider>
-                </IntlProvider>  
-        )
+        if (wasUserConfigSet && !course) {
+            getUserNativeLanguage();
+            if (nativeLanguage) {
+                onLocaleChange(nativeLanguage?.localizationId);
+            }
+            
+            getUserSelectedCourse();
+            if (selectedCourse) {
+                onCourseChange(selectedCourse?.courseAbbreviation);
+            }
+        }
+    }, [wasUserConfigSet, course, currentTheme, nativeLanguage, selectedCourse, onLoadingUserSelectedTheme, switcher, themes, getUserNativeLanguage, onLocaleChange, getUserSelectedCourse, onCourseChange]);
+
+    // Effect to fetch user config flag
+    useEffect(() => {
+        getWasUserConfigSetFlag();
+    }, [getWasUserConfigSetFlag]);
+
+    return (
+        <IntlProvider locale={currentAppLocale.locale} messages={currentAppLocale.messages}>
+            <ConfigProvider locale={currentAppLocale.antd} direction={direction}>
+                <Switch>
+                    {!wasUserConfigSet && <CourseSelection />}
+                    <Route exact path="/">
+                        <Redirect to={APP_PREFIX_PATH ? APP_PREFIX_PATH : DEFAULT_PREFIX_VIEW} />
+                    </Route>
+                    <Route path={APP_PREFIX_PATH ? APP_PREFIX_PATH : DEFAULT_PREFIX_VIEW}>
+                        <AppLayout direction={direction} location={props.location} />
+                    </Route>
+                </Switch>                            
+            </ConfigProvider>
+        </IntlProvider>  
+    );
 }
 
-
-function mapDispatchToProps(dispatch){
-	return bindActionCreators({
-        getWasUserConfigSetFlag: getWasUserConfigSetFlag,
-        getUserSelectedCourse: getUserSelectedCourse,
-        onCourseChange: onCourseChange,
-        getUserNativeLanguage: getUserNativeLanguage,
-        onLocaleChange: onLocaleChange,
-        onLoadingUserSelectedTheme: onLoadingUserSelectedTheme
-	}, dispatch)
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        getWasUserConfigSetFlag,
+        getUserSelectedCourse,
+        onCourseChange,
+        getUserNativeLanguage,
+        onLocaleChange,
+        onLoadingUserSelectedTheme
+    }, dispatch);
 }
 
-// This gets triggered first before it goes to the "const Views"
-// This connects us with Redux to pass the "props" as if it was a Session
 const mapStateToProps = ({ theme, lrn }) => {
     const { wasUserConfigSet, selectedCourse, nativeLanguage } = lrn;
-	const { locale, direction, course, currentTheme, subNavPosition } =  theme;
-	return { locale, direction, course, wasUserConfigSet, selectedCourse, nativeLanguage, currentTheme, subNavPosition }
+    const { locale, direction, course, currentTheme, subNavPosition } = theme;
+    return { locale, direction, course, wasUserConfigSet, selectedCourse, nativeLanguage, currentTheme, subNavPosition };
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Views));
