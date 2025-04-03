0b5125059678de8f51194753b218955c0c6b29db
Alberto Arellano <are11001@byui.edu>
2025-04-03 06:24:05 -0600

    handle safe

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'v0.1.48'; // Increment with every deployment
+const CACHE_NAME = 'v0.1.49'; // Increment with every deployment
 const CACHE_ASSETS = [
   '/',
   '/index.html',
-  '/main.js?v=0.1.48',
-  '/styles.css?v=0.1.48',
+  '/main.js?v=0.1.49',
+  '/styles.css?v=0.1.49',
 ];
 
 // Install event: Cache initial assets
