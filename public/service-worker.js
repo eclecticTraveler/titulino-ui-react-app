207bf233461f3d788f00cc9796ef4bbe3834eec3
Alberto Arellano <are11001@byui.edu>
2025-04-03 05:52:04 -0600

    making changes to yml

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'v0.1.47'; // Increment with every deployment
+const CACHE_NAME = 'v0.1.48'; // Increment with every deployment
 const CACHE_ASSETS = [
   '/',
   '/index.html',
-  '/main.js?v=0.1.47',
-  '/styles.css?v=0.1.47',
+  '/main.js?v=0.1.48',
+  '/styles.css?v=0.1.48',
 ];
 
 // Install event: Cache initial assets
