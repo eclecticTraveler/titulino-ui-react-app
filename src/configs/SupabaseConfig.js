324de9c61b966f23344720b7b363671f52ea6678
Alberto Arellano <are11001@byui.edu>
2025-04-10 08:51:48 -0600

    enviroment

--- src/configs/SupabaseConfig.js
+++ src/configs/SupabaseConfig.js
@@ -1,7 +1,8 @@
 const SupabaseConfig = {
   supabaseProjectUrl: process.env.REACT_APP_SUPABASE_URL,
 	supabaseAnonApiKey: process.env.REACT_APP_SUPABASE_ANON_KEY,
-  baseApiUrl: process.env.REACT_APP_SUPABASE_BASE_API_URL
+  baseApiUrl: process.env.REACT_APP_SUPABASE_BASE_API_URL,
+  enviroment: process.env.REACT_APP_ENV,
 };
 
 export default SupabaseConfig;
