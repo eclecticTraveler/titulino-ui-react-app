0546c80c543ae910fee0a53640b1c1aefef62378
Alberto Arellano <are11001@byui.edu>
2025-07-11 18:08:43 -0600

    app

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'v0.1.84'; // Increment with every deployment
+const CACHE_NAME = 'v0.1.85'; // Increment with every deployment
 const CACHE_ASSETS = [
   '/',
   '/index.html',
-  '/main.js?v=0.1.84',
-  '/styles.css?v=0.1.84',
+  '/main.js?v=0.1.85',
+  '/styles.css?v=0.1.85',
 ];
 
 // Install event: Cache initial assets
