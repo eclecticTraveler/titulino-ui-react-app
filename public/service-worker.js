0499c96c48303a13f67d3b9c53b7a2797f1f0269
Alberto Arellano <are11001@byui.edu>
2025-08-25 12:27:28 -0600

    local data catalog

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v0.1.137'; // Increment with every deployment
+const CACHE_NAME = 'v0.1.138'; // Increment with every deployment
 const CACHE_ASSETS = [
   '/',
   '/index.html'
--- src/services/ShoppingCatalogService.js
+++ src/services/ShoppingCatalogService.js
@@ -1,16 +1,16 @@
-import localShoppingCatalogDataDV from '../assets/data/course-shopping-catalog-data-dv.json';
-import localShoppingCatalogDataPD from '../assets/data/course-shopping-catalog-data.json';
+import localShoppingCatalogData from '../assets/data/course-shopping-catalog-data.json';
 import GoogleService from './GoogleService';
-
 import { env } from "configs/EnvironmentConfig";
 
 const loadLocalShoppingData = async() => {
   console.log("🔄 Loading local shopping catalog data from env.ENVIROMENT...", env.ENVIROMENT);
   if(env.ENVIROMENT === "prod"){
+    console.log("🌐 Loading shopping catalog data from Google Bucket PD...");
     const rawData = await GoogleService.getCourseShoppingCatalogData("loadLocalShoppingData");
     return rawData;
   }else{
-    const rawData = localShoppingCatalogDataDV;
+    console.log("🌐 Loading shopping catalog from local DV...");
+    const rawData = localShoppingCatalogData;
     return rawData;  
   }
 }
